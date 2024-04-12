cef6d812f6bc203b4d3e1e16199406589336f066
leew0nseok <zzang50aa@gmail.com>
2024-04-12 14:53:42 +0900

    Initial commit

---
+++ src/main/java/umc/study/Umc6thApplication.java
@@ -0,0 +1,13 @@
+package umc.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc6thApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Umc6thApplication.class, args);
+	}
+
+}
