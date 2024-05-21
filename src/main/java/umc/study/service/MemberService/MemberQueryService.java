87cada2e310cc22b726882f956ef095685f81246
leew0nseok <zzang50aa@gmail.com>
2024-05-21 12:53:34 +0900

    내가 작성한 리뷰 목록 조회 API 구현

--- src/main/java/umc/study/service/MemberService/MemberQueryService.java
+++ src/main/java/umc/study/service/MemberService/MemberQueryService.java
@@ -1,4 +1,14 @@
 package umc.study.service.MemberService;
 
+import org.springframework.data.domain.Page;
+import umc.study.domain.Member;
+import umc.study.domain.Review;
+import umc.study.domain.Store;
+
+import java.util.Optional;
+
 public interface MemberQueryService {
+    Optional<Member> findMember(Long id);
+
+    Page<Review> getReviewList(Long MemberId, Integer page);
 }
---
+++ src/main/java/umc/study/validation/validator/MemberExistValidator.java
@@ -0,0 +1,38 @@
+package umc.study.validation.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import umc.study.apiPayload.code.status.ErrorStatus;
+import umc.study.repository.MemberRepository;
+import umc.study.validation.annotation.ExistMembers;
+
+
+@Component
+@RequiredArgsConstructor
+public class MemberExistValidator implements ConstraintValidator<ExistMembers, Long> {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public void initialize(ExistMembers constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(Long value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return true; // Null 값은 따로 @NotNull 등의 어노테이션으로 검증합니다.
+        }
+
+        boolean isValid = memberRepository.existsById(value);
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(ErrorStatus.STORE_NOT_FOUND.toString()).addConstraintViolation();
+        }
+
+        return isValid;
+    }
+}
